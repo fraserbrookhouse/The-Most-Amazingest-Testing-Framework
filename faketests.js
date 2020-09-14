08e1e3088f67b40ce0449a68650584a960c5efc2
Fraser Brookhouse <fbrookhouse@gmail.com>
2020-09-14 17:58:34 +0100

    it works

--- faketests.js
+++ faketests.js
@@ -3,5 +3,7 @@ describe('maths', function () {
   it('adds numbers together', function () {
     expect(1 + 1).toBe(2);
   });
+  it('adds large numbers together', function () {
+    expect(1000 + 5264).toBe(6264);
+  })
 });
-
--- tests.js
+++ tests.js
@@ -6,41 +6,49 @@ var category = {};
 var tests = [];
 
 function expect(value) {
-  function toBe(expectedValue) {
+  return {
+    toBe: function(expectedValue) {
     if (value === expectedValue) {
       should.expects.push({
-        expectation: 'expect ${value} to equal ${expectedValue}',
+        expectation: `expect ${value} to equal ${expectedValue}`,
         passed: true,
       });
       passedTests++;
     } else {
       should.expects.push({
-        expectation: 'expect ${value} to equal ${expectedValue}',
+        expectation: `expect ${value} to equal ${expectedValue}`,
         passed: false,
       });
       failedTests++;
     }
   }
 }
+}
 
 function it(desc, fn) {
   should = { description: desc, expects: [] };
   category.it.push(should);
   totalTests++;
+  fn.apply(this)
 }
 
 function describe(desc, fn) {
   category = { it: [] };
   category.description = desc;
   tests.push(category);
+  fn.apply(this)
 }
 
 function runTests() {
-  console.log(tests);
+  console.log(`Total Tests: ${totalTests}, Total Tests Passed: ${passedTests}, Total Tests Failed: ${failedTests}`)
   for (var index = 0; index < tests.length; index++) {
     console.log(tests[index].description)
-    for (var i = 0; i < tests[index].should.length; i++) {
-      console.log(tests[index].should[i].expectation);
+    for (var i = 0; i < tests[index].it.length; i++) {
+      console.log(tests[index].it[i].description);
+      for (var ii = 0; ii < tests[index].it[i].expects.length; ii++) {
+        console.log(tests[index].it[i].expects[ii].expectation);
+        console.log('Passed? ' + tests[index].it[i].expects[ii].passed);
+      }
     }
   }
 }
