38d99b28db34175713ccf9b20e8a031a700ef8f1
Fraser Brookhouse <fbrookhouse@gmail.com>
2020-09-15 14:38:07 +0100

    made tests autorun on specrunner page load

--- spec/note-model-test.js
+++ spec/note-model-test.js
@@ -1,6 +1,6 @@
 describe(Note, function () {
   it('should have a text property after being created', function () {
     var testNote = new Note('This is a test note');
-    expect(testNote.getText()).toBe('This is a test note')
-  })
-})
+    expect(testNote.getText()).toBe('This is a test note');
+  });
+});
--- spec/tests.js
+++ spec/tests.js
@@ -7,42 +7,44 @@ var tests = [];
 
 function expect(value) {
   return {
-    toBe: function(expectedValue) {
-    if (value === expectedValue) {
-      should.expects.push({
-        expectation: `expect ${value} to equal ${expectedValue}`,
-        passed: true,
-      });
-      passedTests++;
-    } else {
-      should.expects.push({
-        expectation: `expect ${value} to equal ${expectedValue}`,
-        passed: false,
-      });
-      failedTests++;
-    }
-  }
-}
+    toBe: function (expectedValue) {
+      if (value === expectedValue) {
+        should.expects.push({
+          expectation: `expect ${value} to equal ${expectedValue}`,
+          passed: true,
+        });
+        passedTests++;
+      } else {
+        should.expects.push({
+          expectation: `expect ${value} to equal ${expectedValue}`,
+          passed: false,
+        });
+        failedTests++;
+      }
+    },
+  };
 }
 
 function it(desc, fn) {
   should = { description: desc, expects: [] };
   category.it.push(should);
   totalTests++;
-  fn.apply(this)
+  fn.apply(this);
 }
 
 function describe(desc, fn) {
   category = { it: [] };
   category.description = desc;
   tests.push(category);
-  fn.apply(this)
+  fn.apply(this);
 }
 
 function runTests() {
-  console.log(`Total Tests: ${totalTests}, Total Tests Passed: ${passedTests}, Total Tests Failed: ${failedTests}`)
+  console.log(
+    `Total Tests: ${totalTests}, Total Tests Passed: ${passedTests}, Total Tests Failed: ${failedTests}`
+  );
   for (var index = 0; index < tests.length; index++) {
-    console.log(tests[index].description)
+    console.log(tests[index].description);
     for (var i = 0; i < tests[index].it.length; i++) {
       console.log(tests[index].it[i].description);
       for (var ii = 0; ii < tests[index].it[i].expects.length; ii++) {
