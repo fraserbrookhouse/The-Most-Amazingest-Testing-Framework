180312b92eda9998c9d6a2dcfdd213410fac4984
Fraser Brookhouse <fbrookhouse@gmail.com>
2020-09-15 15:29:44 +0100

    added failing test for NoteListView class to return the text of multiple notes in the NoteList it is passed in structured html

--- spec/note-list-view-test.js
+++ spec/note-list-view-test.js
@@ -1,15 +1,24 @@
 describe(NoteListView, function () {
   it('it stores a NoteList object with no notes and returns view with only ul tags', function () {
-    var notelist = new NoteList()
-    var notelistview = new NoteListView(notelist)
-    expect(notelistview.returnView()).toBe('<ul></ul>')
-  })
+    var notelist = new NoteList();
+    var notelistview = new NoteListView(notelist);
+    expect(notelistview.returnView()).toBe('<ul></ul>');
+  });
   it('it stores a NoteList object with one note, and returns the note in structured html', function () {
     var notelist = new NoteList();
-    notelist.createNote('This is a test note')
+    notelist.createNote('This is a test note');
     var notelistview = new NoteListView(notelist);
     expect(notelistview.returnView()).toBe(
       '<ul><li><div>This is a test note</div></li></ul>'
     );
   });
-})
+  it('it stores a NoteList object with more than one note, and returns the notes in structured html', function () {
+    var notelist = new NoteList();
+    notelist.createNote('This is a test note');
+    notelist.createNote('This is also a test note');
+    var notelistview = new NoteListView(notelist);
+    expect(notelistview.returnView()).toBe(
+      '<ul><li><div>This is a test note</div></li><li><div>This is also a test note</div></li></ul>'
+    );
+  });
+});
