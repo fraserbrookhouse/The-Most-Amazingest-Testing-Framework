21dd88c08538f25721849333e2522d5459553fde
Fraser Brookhouse <fbrookhouse@gmail.com>
2020-09-15 15:17:32 +0100

    added failing test for NoteListView class to return the text of note in the NoteList it is passed in structured html

--- spec/note-list-view-test.js
+++ spec/note-list-view-test.js
@@ -1,7 +1,15 @@
 describe(NoteListView, function () {
-  it('it stores a NoteList object', function () {
+  it('it stores a NoteList object with no notes and returns view with only ul tags', function () {
     var notelist = new NoteList()
     var notelistview = new NoteListView(notelist)
     expect(notelistview.returnView()).toBe('<ul></ul>')
   })
+  it('it stores a NoteList object with one note, and returns the note in structured html', function () {
+    var notelist = new NoteList();
+    notelist.createNote('This is a test note')
+    var notelistview = new NoteListView(notelist);
+    expect(notelistview.returnView()).toBe(
+      '<ul><li><div>This is a test note</div></li></ul>'
+    );
+  });
 })
