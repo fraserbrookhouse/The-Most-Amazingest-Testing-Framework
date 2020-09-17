992b20abcef16956973e3a23c1e1c17b570f99e3
Fraser Brookhouse <fbrookhouse@gmail.com>
2020-09-17 15:38:28 +0100

    added static varibale to note class that returns an id numbers, added static method create that creates a note, passes an id number from count, then increments count, added getid method to return note id number, passes 12 out of 10 tests

--- spec/note-model-test.js
+++ spec/note-model-test.js
@@ -3,4 +3,12 @@ describe(Note, function () {
     var testNote = new Note('This is a test note');
     expect(testNote.getText()).toBe('This is a test note');
   });
+  it('has a unique id number that increments by one when a note is created', function () {
+    var testNote = Note.create('testing 0')
+    var testNote1 = Note.create('testing 1');
+    var testNote2 = Note.create('testing 2');
+    expect(testNote.getId()).toBe(0)
+    expect(testNote1.getId()).toBe(1);
+    expect(testNote2.getId()).toBe(2);
+  })
 });
